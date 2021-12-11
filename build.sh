d4d85d641fd3630183f1eb849bba4cf7258ffe0b
Federico Devigili <f.devigili@alpitronic.it>
2021-12-11 08:34:09 +0100

    update

--- build.sh
+++ build.sh
@@ -3,14 +3,23 @@
 ../qt5/configure \
 --opensource -confirm-license \
 -device imx6 \
+-release \
+-force-pkg-config \
+-v \
 -prefix /usr/local/qt5 \
 -extprefix ./buildOut/extprefix \
--hostprefix ./buildOut/hostprefix/ \
+-hostprefix ./buildOut/hostprefix \
 -device-option CROSS_COMPILE=/opt/fslc-framebuffer/2.4.1/sysroots/x86_64-fslcsdk-linux/usr/bin/arm-fslc-linux-gnueabi/arm-fslc-linux-gnueabi- \
 -sysroot /opt/fslc-framebuffer/2.4.1/sysroots/armv7at2hf-neon-fslc-linux-gnueabi \
--release \
--make libs \
--nomake examples -no-icu -nomake tests  -no-feature-openssl  -skip qtwebengine \
--no-pch -no-xcb -make libs -release -opengl es2 -egl -eglfs -v \
--skip serialport \
+-tslib \
+-libinput \
+-xkbcommon \
+-skip qtwebengine \
+-skip qtgamepad \
+-skip qtlocation \
+-skip qtwayland \
+-nomake examples \
+-nomake tests \
+-no-pch \
+-no-openssl \
 
